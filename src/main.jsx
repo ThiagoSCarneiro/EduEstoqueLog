34f9e73beb7e45f8b7fda980b67bc7e279ccc788
Rafael Cajazeira <147667168+rcajazeira@users.noreply.github.com>
2025-05-08 11:29:26 -0300

    Alterações Dev/Rafael

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,18 @@
-import { StrictMode } from 'react'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Pages from './pages/pages.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Pages />
+  </React.StrictMode>,
+);
+
+
+
+
+
+/* import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import '@fontsource/inter'
 
@@ -6,4 +20,4 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+) */
